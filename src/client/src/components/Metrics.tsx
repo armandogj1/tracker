d3b516bc151fa3bc7380802e25e1985e7539f70f
Jorge Armando Garcia-Jacquier <j.armando.garcia.jacquier@gmail.com>
2021-04-04 22:49:46 -0400

    Include Line graph, Add padding css

--- src/client/src/components/Metrics.tsx
+++ src/client/src/components/Metrics.tsx
@@ -1,7 +1,7 @@
 import React, { MutableRefObject, useEffect, useRef, useState } from 'react';
 import { useQueryClient } from 'react-query';
 import { ITicket } from '../API_Helpers/Board';
-import { Bar } from 'react-chartjs-2';
+import { Bar, Line } from 'react-chartjs-2';
 
 const Metrics = () => {
   const queryClient = useQueryClient();
@@ -17,10 +17,75 @@ const Metrics = () => {
   const applicationsCount = statuses.map((status) => {
     return Number(Object.values(tickets).filter((t) => t.status === status).length);
   });
-
   const colors = ['#7777ff', '#77adff', '#77fdff', '#77ff9e', '#a0ff77', '#ff9777'];
 
-  const chartData = {
+  const dateLabels = Object.values(tickets)
+    .reduce((acc, { timestamps }) => {
+      const d: number[] = [];
+
+      Object.values(timestamps).forEach((time) => {
+        d.push(Date.parse(time + ''));
+      });
+
+      return [...acc, ...d];
+    }, [] as number[])
+    .sort((a, b) => a - b)
+    .reduce((unique, d) => {
+      const dateObj = new Date(d);
+      const date = `${dateObj.getMonth()}/${
+        dateObj.getDate() + 1
+      }/${dateObj.getFullYear()}`;
+
+      if (unique[unique.length - 1] !== date) {
+        unique.push(date);
+      }
+
+      return unique;
+    }, [] as string[]);
+
+  const datesMap = Object.values(tickets).reduce((acc, tix) => {
+    Object.entries(tix.timestamps).forEach(([status, time]) => {
+      const dateObj = new Date(time);
+      const date = `${dateObj.getMonth()}/${
+        dateObj.getDate() + 1
+      }/${dateObj.getFullYear()}`;
+
+      if (!acc.has(status)) {
+        acc.set(status, {});
+      }
+
+      const dateCounts = acc.get(status);
+      dateCounts[date] = dateCounts[date] ? dateCounts[date] + 1 : 1;
+    });
+
+    return acc;
+  }, new Map());
+
+  const dateData = statuses.map((status, idx) => {
+    if (datesMap instanceof Map) {
+      const dateCountsForStatus = datesMap.get(status);
+      const dataObjs = [];
+
+      for (let date in dateCountsForStatus) {
+        dataObjs.push({ x: date, y: dateCountsForStatus[date] });
+      }
+
+      return {
+        label: status,
+        data: dataObjs,
+        fill: false,
+        borderColor: colors[idx],
+      };
+    }
+    return {};
+  });
+
+  const lineData = {
+    labels: dateLabels,
+    datasets: dateData,
+  };
+
+  const barChartData = {
     labels: statuses,
     datasets: [
       {
@@ -33,12 +98,17 @@ const Metrics = () => {
 
   return (
     <section className='metrics'>
-      <Bar
-        data={chartData}
-        width={500}
-        height={400}
-        options={{ maintainAspectRatio: false }}
-      />
+      <div className='graph'>
+        <Bar data={barChartData} options={{ maintainAspectRatio: false }} />
+      </div>
+      <div className='graph'>
+        <Line
+          data={lineData}
+          options={{
+            maintainAspectRatio: false,
+          }}
+        />
+      </div>
     </section>
   );
 };
