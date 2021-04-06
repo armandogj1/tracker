d567476193a1ce15f88b446a94d54f1e5d2a6513
Jorge Armando Garcia-Jacquier <j.armando.garcia.jacquier@gmail.com>
2021-04-06 00:00:18 -0400

    Add roadmap for notifications in README

--- src/client/src/helpers/notifications.ts
+++ src/client/src/helpers/notifications.ts
@@ -1,6 +1,5 @@
 const testNotification = async () => {
   if (!Notification) return console.log('no notification');
-  console.log('this is in test');
 
   if (Notification.permission === 'default') {
     await Notification.requestPermission();
